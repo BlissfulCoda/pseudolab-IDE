b99e2f6b154a161a1bc686c27828a39152ef6060
Ronnie Kiyegga <ronniekiyegga@hotmail.com>
2026-03-05 16:14:30 +0000

    fix(layout): updated the navigation component to fix a hydration error

--- app/components/Navigation.tsx
+++ app/components/Navigation.tsx
@@ -2,19 +2,29 @@ import Link from "next/link";
 
 const Navigation = () => {
   return (
-    <div className="w-full m-4">
+    <nav className="w-full m-4">
       <ul className="flex gap-4 text-sm group text-gray-300">
-        <Link href="/" className="group-hover:text-gray-800">
-          Features
-        </Link>
-        <Link href="/pages/ai-tutor" className="group-hover:text-gray-800">
-          AI Tutor
-        </Link>
-        <Link href="/pages/references">Reference</Link>
-        <Link href="/pages/how-it-works">How it works</Link>
-        <Link href="/pages/specifications">Cambridge Specs</Link>
+        <li>
+          <Link href="/" className="group-hover:text-gray-800">
+            Features
+          </Link>
+        </li>
+        <li>
+          <Link href="/pages/ai-tutor" className="group-hover:text-gray-800">
+            AI Tutor
+          </Link>
+        </li>
+        <li>
+          <Link href="/pages/references">Reference</Link>
+        </li>
+        <li>
+          <Link href="/pages/how-it-works">How it works</Link>
+        </li>
+        <li>
+          <Link href="/pages/specifications">Cambridge Specs</Link>
+        </li>
       </ul>
-    </div>
+    </nav>
   );
 };
 
